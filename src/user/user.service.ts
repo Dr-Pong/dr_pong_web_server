e643d0f17888bf462a1c7c08156f1ee0fc9107cd
ManOfDaemosan <hwanee0000@gmail.com>
2023-04-06 17:56:11 +0900

    [Feat / Api] 유저 기본 정보 조회, 수정 #1

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -3,7 +3,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './user.entity';
 import { Repository } from 'typeorm';
 import { UsersDetailDto } from './dto/users.detail.dto';
-import { UserTitle } from 'src/user-title/usertitle.entity';
+import { UserTitle } from 'src/user-title/user-title.entity';
 import { PatchUsersDetailDto } from './dto/patch.users.detail.dto';
 import { UsersTitlesDto } from './dto/users.title.dto';
 import { UsersAchievementDto } from './dto/users.achievement.dto';
@@ -14,7 +14,7 @@ export class UserService {
     @InjectRepository(User)
     private userRepository: Repository<User>,
     private userTitleRepository: Repository<UserTitle>,
-	private userAcheivementsRepository: Repository<>//achievement entity
+    private userAcheivementsRepository: Repository<>, //achievement entity
   ) {}
 
   async userDetailByNicknameGet(nickname: string): Promise<UsersDetailDto> {
@@ -54,34 +54,4 @@ export class UserService {
     await this.userRepository.save(user);
     await this.userTitleRepository.save(userTitle);
   } /* detail fetch함수 */
-
-  async userTitleGet(nickname: string): Promise<UsersTitlesDto> {
-    const user = await this.userRepository.findOne({
-      where: { nickname: nickname },
-    });
-    if (!user) throw new NotFoundException('No such User');
-    const userTitle = await this.userTitleRepository.findOne({
-      where: { user: { id: user.id }, isSelected: true },
-    });
-    const responseDto: UsersTitlesDto = {
-      id: user.id,
-      title: userTitle.title.name,
-    };
-    return responseDto;
-  } /*title get함수 */
-
-  async userAcheivementsGet(
-    nickname: string,
-    isSelected: boolean,
-  ): Promise<UsersAchievementDto> {
-    const user = await this.userRepository.findOne({
-      where: { nickname: nickname },
-    });
-    if (!user) throw new NotFoundException('No such User');
-	const usersAchievement = await 
-    const responseDto: UsersAchievementDto = {
- 
-    };
-    return responseDto;
-  } /*achievement get함수 */
 }
