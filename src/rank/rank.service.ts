7f0e75ad75c0de7fff658dff22b51222df65f5ab
ManOfDaemosan <hwanee0000@gmail.com>
2023-04-21 18:06:46 +0900

    [Feat / Service] 불안정한 서비스로직 #34

--- src/rank/rank.service.ts
+++ src/rank/rank.service.ts
@@ -1,4 +1,54 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Rank } from './rank.entity';
+import { Season } from 'src/season/season.entity';
+import { GetUserRankStatDto } from './dto/get.user.rank.stat.dto';
+import { UserRankStatDto } from './dto/user.rank.stat.dto';
+import { GetUserBestRankStatDto } from './dto/get.user.best.rnak.stat.dto';
 
 @Injectable()
-export class RankService {}
+export class RankService {
+  constructor(
+    @InjectRepository(Season)
+    private seasonRepository: Repository<Season>,
+    @InjectRepository(Rank)
+    private rankRepository: Repository<Rank>,
+  ) {}
+  //get
+  async getUserRankBySeason(
+    getDto: GetUserRankStatDto,
+  ): Promise<UserRankStatDto> {
+    const userRanks = await this.rankRepository.findOne({
+      where: { user: { id: getDto.userId }, season: { id: getDto.seasonId } },
+    });
+    if (!userRanks) {
+      throw new BadRequestException('No Rank Data');
+    }
+    const userRank = userRanks.ladderRank;
+    const userRecord = userRanks.ladderPoint;
+    const responseDto: UserRankStatDto = {
+      rank: userRank,
+      record: userRecord,
+    };
+    return responseDto;
+  }
+
+  async getUserBestRank(
+    getDto: GetUserBestRankStatDto,
+  ): Promise<UserRankStatDto> {
+    const userRanks = await this.rankRepository.findOne({
+      where: { user: { id: getDto.userId } },
+    });
+    if (!userRanks) {
+      throw new BadRequestException('No Rank Data');
+    }
+    const userBestRank = userRanks.highestRanking;
+    const userBestRecord = userRanks.highestPoint;
+    const responseDto: UserRankStatDto = {
+      rank: userBestRank,
+      record: userBestRecord,
+    };
+    return responseDto;
+  }
+}
