38b57f0ceab5fadb36d0c6d28adccdcd240219e7
Heo Namjun <heonamjun@Heos-MacBook-Air.local>
2023-04-25 13:35:43 +0900

    [Fix] FtStrategy 제거 #16

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
-import { FortyTwoStrategy } from './auth.fortytwo.strategy';
+import { AuthService } from './auth.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from 'src/user/user.entity';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { JwtStrategy } from './jwt/auth.jwt.strategy';
 
 @Module({
+  imports: [
+    PassportModule.register({defaultStrategy:'jwt'}),
+    JwtModule.register({
+      secret:'jwtSecret',
+      signOptions:{
+        expiresIn: 60 * 60 * 60,
+      }
+    }),
+    TypeOrmModule.forFeature([User])
+  ],
   controllers: [AuthController],
-  providers:[FortyTwoStrategy]
+  providers:[AuthService, JwtStrategy],
+  exports:[JwtStrategy, PassportModule],
 })
 export class AuthModule {}
