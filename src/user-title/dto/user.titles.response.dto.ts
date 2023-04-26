2b35a92e545a99f8969b4d60eda94a3325950fdb
Heo Namjun <heonamjun@Heos-MacBook-Air.local>
2023-04-26 11:21:20 +0900

    [Feat] 명세에 따른 response dto 생성

---
+++ src/user-title/dto/user.titles.response.dto.ts
@@ -0,0 +1,5 @@
+import { UserTitlesDto } from "./user.titles.dto";
+
+export class UserTitlesResponseDto {
+	titles: UserTitlesDto;
+}
--- src/user/dto/user.titles.dto.ts
+++
@@ -1,9 +0,0 @@
-export class UserTitleDto {
-  id: number;
-
-  title: string;
-}
-
-export class UserTitlesDto {
-  titles: UserTitleDto[];
-}
