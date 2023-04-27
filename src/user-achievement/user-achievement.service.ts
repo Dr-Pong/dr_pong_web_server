2850a8a0b240a7323985bc2560a42bc1e962f37d
ManOfDaemosan <hwanee0000@gmail.com>
2023-04-27 12:07:58 +0900

    [Refector] camelCase로 Patch 요청 칭호 이모지 타이틀 대통일 #1

--- src/user-achievement/user-achievement.service.ts
+++ src/user-achievement/user-achievement.service.ts
@@ -20,83 +20,84 @@ export class UserAchievementService {
     @InjectRepository(Achievemet)
     private achievementRepository: Repository<Achievemet>,
   ) {}
+
   async getUserAchievementsAll(
     getDto: GetUserAchievementsDto,
   ): Promise<UserAchievementsDto> {
-      //achieved이고 selected되지 않은 업적 return
-      const allAchievement = await this.achievementRepository.find();
-      const userAchievement = await this.userAchievementRepository.find({
-        where: { user: { id: getDto.userId } },
+    //achieved이고 selected되지 않은 업적 return
+    const allAchievement = await this.achievementRepository.find();
+    const userAchievement = await this.userAchievementRepository.find({
+      where: { user: { id: getDto.userId } },
+    });
+    const achievements: UserAchievementDto[] = [];
+    const status = new UserCollectablesStatus(allAchievement.length);
+    status.setStatus(userAchievement); // userAchievement 에 있는 status 를 allAchievement 에 변경
+
+    for (const c of allAchievement) {
+      achievements.push({
+        id: c.id,
+        name: c.name,
+        status: status.getStatus(c.id),
       });
-      const achievements: UserAchievementDto[] = [];
-      const status = new UserCollectablesStatus(allAchievement.length);
-      status.setStatus(userAchievement); // userAchievement 에 있는 status 를 allAchievement 에 변경
+    }
 
-      for (const c of allAchievement) {
-        achievements.push({
-          id: c.id,
-          name: c.name,
-          status: status.getStatus(c.id),
-        });
-      }
-
-      const responseDto: UserAchievementsDto = {
-        achievements: achievements,
-      };
-      return responseDto;
+    const responseDto: UserAchievementsDto = {
+      achievements: achievements,
+    };
+    return responseDto;
   }
 
   //get user achievements
   async getUserAchievementsSelected(
     getDto: GetUserAchievementsDto,
   ): Promise<UserAchievementsDto> {
-      //achieved이고 selected된 업적 return
-      const selectAchievement = await this.userAchievementRepository.find({
-        where: { user: { id: getDto.userId }, selectedOrder: Not(IsNull()) },
-      });
-      const achievements : UserAchievementDto[] = [null, null, null];
-      for (const userAchievement of selectAchievement) {
-        achievements[userAchievement.selectedOrder] = {
-          id: userAchievement.achievement.id,
-          name: userAchievement.achievement.name,
-          status: CollectableStatus.SELECTED,
-        };
-      }
-      const responseDto: UserAchievementsDto = {
-        achievements: achievements,
+    //achieved이고 selected된 업적 return
+    const selectAchievement = await this.userAchievementRepository.find({
+      where: { user: { id: getDto.userId }, selectedOrder: Not(IsNull()) },
+    });
+    const achievements: UserAchievementDto[] = [null, null, null];
+    for (const userAchievement of selectAchievement) {
+      achievements[userAchievement.selectedOrder] = {
+        id: userAchievement.achievement.id,
+        name: userAchievement.achievement.name,
+        status: CollectableStatus.SELECTED,
       };
-      return responseDto;
+    }
+    const responseDto: UserAchievementsDto = {
+      achievements: achievements,
+    };
+    return responseDto;
   }
 
   //patch user achievement Patch 가 old 랑 to_change로 저장하고 return 하는 로직을 구현해야한다
   async patchUserAchievements(
     patchDto: PatchUserAchievementsDto,
   ): Promise<void> {
-    const old_achievements: UserAchievement[] =
+    const oldAchievements: UserAchievement[] =
       await this.userAchievementRepository.find({
         where: {
           user: { id: patchDto.userId },
           selectedOrder: Not(IsNull()),
         },
       });
-    const to_change_achievement: UserAchievement[] =
+    const toChangeAchievement: UserAchievement[] =
       await this.userAchievementRepository.find({
         where: {
           user: { id: patchDto.userId },
-          achievement: {id:In(patchDto.achievementsId)}
+          achievement: { id: In(patchDto.achievementsId) },
         },
       });
     const countNumbers = patchDto.achievementsId.filter(
       (elem) => typeof elem === 'number',
     ).length;
-    if (countNumbers !== to_change_achievement.length) {
+    if (countNumbers !== toChangeAchievement.length) {
       throw new BadRequestException('No such achievement');
     }
 
-    for (const c of to_change_achievement) {
+    for (const c of toChangeAchievement) {
       c.selectedOrder = null;
     }
-    for (const c of to_change_achievement) {
+    for (const c of toChangeAchievement) {
       let i = 0;
       for (const d of patchDto.achievementsId) {
         if (c.achievement.id === d) {
@@ -106,7 +107,7 @@ export class UserAchievementService {
         i++;
       }
     }
-    await this.userAchievementRepository.save(old_achievements);
-    await this.userAchievementRepository.save(to_change_achievement);
+    await this.userAchievementRepository.save(oldAchievements);
+    await this.userAchievementRepository.save(toChangeAchievement);
   }
 }
