21f73063e2d1ea1bfdfdaaecabcf348bc71a5149
Heo Namjun <heonamjun@Heos-MacBook-Air.local>
2023-05-01 19:30:17 +0900

    [Fix] roletype 타입 수정

--- src/global/type/type.user.roletype.ts
+++ src/global/type/type.user.roletype.ts
@@ -1,1 +1,6 @@
+export const ROLETYPE_GUEST : 'guest' = 'guest';
+export const ROLETYPE_NONAME : 'noname' = 'noname';
+export const ROLETYPE_MEMBER : 'member' = 'member';
+export const ROLETYPE_ADMIN : 'admin' = 'admin';
+
 export type RoleType = 'guest' | 'noname' | 'member' | 'admin';
