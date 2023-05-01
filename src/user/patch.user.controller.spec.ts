ea021fdaa855a2498a6e056f21770568c8c2a33a
ManOfDaemosan <hwanee0000@gmail.com>
2023-05-01 21:19:03 +0900

    [Feat/Test] detail Error Case #72

--- src/user/patch.user.controller.spec.ts
+++ src/user/patch.user.controller.spec.ts
@@ -46,7 +46,7 @@ describe('UserController', () => {
     await app.close();
   });
 
-  describe('UserController', () => {
+  describe('PATCH cases', () => {
     let app: INestApplication;
     let dataSources: DataSource;
     let testService: TestService;
@@ -82,10 +82,276 @@ describe('UserController', () => {
       await app.close();
     });
 
-    describe('PATCH cases', () => {
+    describe('/users/{nickname}/detail', () => {
+      it('nickname 의 titile이 null인경우', async () => {
+        const user: User = await testService.createBasicUser();
+        const token = jwtService.sign({
+          id: user.id,
+          nickname: user.nickname,
+          roleType: user.roleType,
+        });
+        const response = await request(app.getHttpServer())
+          .patch('/users/' + user.nickname + '/detail')
+          .set({ Authorization: 'Bearer ' + token })
+          .send({
+            imgUrl: 'changed',
+            title: null,
+            message: 'change message',
+          });
+        // console.log(response.body);
+        const result = await userTitleRepository.findOne({
+          where: { user: { id: user.id }, isSelected: true },
+        });
+
+        // console.log(response.body.title);
+        expect(response.statusCode).toBe(200);
+        expect(result).toBe(null);
+      });
+
+      it('nickname 의 title이 있는경우', async () => {
+        const user: User = await testService.createUserWithSelectedTitles();
+        const token = jwtService.sign({
+          id: user.id,
+          nickname: user.nickname,
+          roleType: user.roleType,
+        });
+        const response = await request(app.getHttpServer())
+          .patch('/users/' + user.nickname + '/detail')
+          .set({ Authorization: 'Bearer ' + token })
+          .send({
+            imgUrl: 'changed',
+            title: testService.titles[0].id,
+            message: 'change message',
+          });
+        // console.log(response.statusCode);
+        expect(response.statusCode).toBe(200);
+        const result = await userTitleRepository.findOne({
+          where: { user: { id: user.id }, isSelected: true },
+        });
+        // console.log(result);
+        expect(result.title.id).toBe(testService.titles[0].id);
+      });
+    });
+
+    describe('/users/{nickname}/achievements', () => {
+      it('achievements를 순서대로 선택한 경우', async () => {
+        const user: User = await testService.createUserWithCollectables();
+        const token = jwtService.sign({
+          id: user.id,
+          nickname: user.nickname,
+          roleType: user.roleType,
+        });
+
+        const response = await request(app.getHttpServer())
+          .patch('/users/' + user.nickname + '/achievements')
+          .set({ Authorization: 'Bearer ' + token })
+          .send({
+            achievements: [
+              testService.achievements[0].id,
+              testService.achievements[1].id,
+              testService.achievements[2].id,
+            ],
+          });
+
+        // console.log(response.statusCode);
+        expect(response.statusCode).toBe(200);
+        const result = await userAchievementRepository.find({
+          where: { user: { id: user.id } },
+        });
+        // console.log(result);
+        expect(result[0].achievement.id).toBe(testService.achievements[0].id);
+        expect(result[1].achievement.id).toBe(testService.achievements[1].id);
+        expect(result[2].achievement.id).toBe(testService.achievements[2].id);
+      });
+
+      it('achievement을 임의의 순서대로 선택한경우', async () => {
+        const user: User =
+          await testService.createMixedSelectedAchievementUser();
+        const token = jwtService.sign({
+          id: user.id,
+          nickname: user.nickname,
+          roleType: user.roleType,
+        });
+        const response = await request(app.getHttpServer())
+          .patch('/users/' + user.nickname + '/achievements')
+          .set({ Authorization: 'Bearer ' + token })
+          .send({
+            achievements: [
+              testService.achievements[2].id,
+              null,
+              testService.achievements[3].id,
+            ],
+          });
+        // console.log(response.statusCode);
+        const result = await userAchievementRepository.find({
+          where: { user: { id: user.id }, selectedOrder: Not(IsNull()) },
+        });
+        // console.log('response', response.body);
+        expect(response.statusCode).toBe(200);
+        expect(result[0].achievement.id).toBe(testService.achievements[2].id);
+        expect(result[1].achievement.id).toBe(testService.achievements[3].id);
+
+        expect(result[0].selectedOrder).toBe(0);
+        expect(result[1].selectedOrder).toBe(2);
+      });
+
+      it('achievement을 선택하지 않은 경우 (전부 null)', async () => {
+        const user: User =
+          await testService.createUserWithUnSelectedAchievements();
+        const token = jwtService.sign({
+          id: user.id,
+          nickname: user.nickname,
+          roleType: user.roleType,
+        });
+        const response = await request(app.getHttpServer())
+          .patch('/users/' + user.nickname + '/achievements')
+          .set({ Authorization: 'Bearer ' + token })
+          .send({
+            achievements: [null, null, null],
+          });
+        // console.log(response.body);
+        const result = await userAchievementRepository.find({
+          where: { user: { id: user.id }, selectedOrder: Not(IsNull()) },
+        });
+        // console.log(result);
+        expect(response.statusCode).toBe(200);
+        expect(result.length).toBe(0);
+      });
+    });
+
+    describe('/users/{nickname}/emojis', () => {
+      it('emoji를 순서대로 선택한 경우', async () => {
+        const user: User = await testService.createUserWithCollectables();
+        const token = jwtService.sign({
+          id: user.id,
+          nickname: user.nickname,
+          roleType: user.roleType,
+        });
+
+        const response = await request(app.getHttpServer())
+          .patch('/users/' + user.nickname + '/emojis')
+          .set({ Authorization: 'Bearer ' + token })
+          .send({
+            emojis: [
+              testService.emojis[0].id,
+              testService.emojis[1].id,
+              testService.emojis[2].id,
+            ],
+          });
+
+        // console.log(response.statusCode);
+        expect(response.statusCode).toBe(200);
+        const result = await userEmojiRepository.find({
+          where: { user: { id: user.id } },
+        });
+        // console.log(result);
+        expect(result[0].emoji.id).toBe(testService.emojis[0].id);
+        expect(result[1].emoji.id).toBe(testService.emojis[1].id);
+        expect(result[2].emoji.id).toBe(testService.emojis[2].id);
+
+        expect(result[0].selectedOrder).toBe(0);
+        expect(result[1].selectedOrder).toBe(1);
+        expect(result[2].selectedOrder).toBe(2);
+      });
+
+      it('emoji를 임의의 순서대로 선택한경우', async () => {
+        const user: User = await testService.createMixedSelectedEmojiUser();
+        const token = jwtService.sign({
+          id: user.id,
+          nickname: user.nickname,
+          roleType: user.roleType,
+        });
+        const response = await request(app.getHttpServer())
+          .patch('/users/' + user.nickname + '/emojis')
+          .set({ Authorization: 'Bearer ' + token })
+          .send({
+            emojis: [testService.emojis[2].id, null, testService.emojis[3].id],
+          });
+
+        const result = await userEmojiRepository.find({
+          where: { user: { id: user.id }, selectedOrder: Not(IsNull()) },
+        });
+        expect(response.statusCode).toBe(200);
+        expect(result[0].emoji.id).toBe(testService.emojis[2].id);
+        expect(result[1].emoji.id).toBe(testService.emojis[3].id);
+
+        expect(result[0].selectedOrder).toBe(0);
+        expect(result[1].selectedOrder).toBe(2);
+      });
+
+      it('emoji를 선택하지 않은 경우 (전부 null)', async () => {
+        const user: User = await testService.createUserWithUnSelectedEmojis();
+        const token = jwtService.sign({
+          id: user.id,
+          nickname: user.nickname,
+          roleType: user.roleType,
+        });
+        const response = await request(app.getHttpServer())
+          .patch('/users/' + user.nickname + '/emojis')
+          .set({ Authorization: 'Bearer ' + token })
+          .send({
+            emojis: [null, null, null],
+          });
+
+        const result = await userEmojiRepository.find({
+          where: { user: { id: user.id }, selectedOrder: Not(IsNull()) },
+        });
+        expect(response.statusCode).toBe(200);
+        expect(result.length).toBe(0);
+      });
+    });
+    describe('patch Error Cases Test', () => {
       describe('/users/{nickname}/detail', () => {
-        it('nickname 의 titile이 null인경우', async () => {
-          const user: User = await testService.createBasicUser();
+        it('존재하지 않는 유저의 경우', async () => {
+          const basicUser = await testService.createBasicUser();
+          const token = jwtService.sign({
+            id: basicUser.id,
+            nickname: basicUser.nickname,
+            roleType: basicUser.roleType,
+          });
+
+          const response = await request(app.getHttpServer())
+            .patch('/users/' + 'nonono' + '/detail')
+            .set({ Authorization: 'Bearer ' + token })
+            .send({
+              nickname: 'testNickname',
+              imagUrl: 'testImageUrl',
+              title: 'testTitle',
+            });
+          // console.log(response.body);
+
+          expect(response.statusCode).toBe(404);
+        });
+
+        it('nickname이 null인 경우', async () => {
+          const basicUser = await testService.createBasicUser();
+          const token = jwtService.sign({
+            id: basicUser.id,
+            nickname: basicUser.nickname,
+            roleType: basicUser.roleType,
+          });
+
+          const response = await request(app.getHttpServer())
+            .patch('/users/' + null + '/detail')
+            .set({ Authorization: 'Bearer ' + token })
+            .send({
+              nickname: 'testNickname',
+              imagUrl: 'testImageUrl',
+              title: 'testTitle',
+            });
+          expect(response.statusCode).toBe(404);
+        });
+
+        // it('유저에게 없는 imageUrl를 요청한 경우', async () => {
+        //   const user: User = await testService.createUserWithCollectables();
+        //   const response = await request(app.getHttpServer()).get(
+        //     '/users/' + user.nickname + '/detail',
+        //   );
+        //   expect(response.statusCode).toBe(200);
+        // });
+
+        it('유저에게 없는 title을 요청한 경우', async () => {
+          const user: User = await testService.createUserWithCollectables();
           const token = jwtService.sign({
             id: user.id,
             nickname: user.nickname,
@@ -95,213 +361,13 @@ describe('UserController', () => {
             .patch('/users/' + user.nickname + '/detail')
             .set({ Authorization: 'Bearer ' + token })
             .send({
-              imgUrl: 'changed',
-              title: null,
-              message: 'change message',
-            });
-          // console.log(response.body);
-          const result = await userTitleRepository.findOne({
-            where: { user: { id: user.id }, isSelected: true },
-          });
-
-          // console.log(response.body.title);
-          expect(response.statusCode).toBe(200);
-          expect(result).toBe(null);
-        });
-
-        it('nickname 의 title이 있는경우', async () => {
-          const user: User = await testService.createUserWithSelectedTitles();
-          const token = jwtService.sign({
-            id: user.id,
-            nickname: user.nickname,
-            roleType: user.roleType,
-          });
-          const response = await request(app.getHttpServer())
-            .patch('/users/' + user.nickname + '/detail')
-            .set({ Authorization: 'Bearer ' + token })
-            .send({
-              imgUrl: 'changed',
-              title: testService.titles[0].id,
-              message: 'change message',
-            });
-          // console.log(response.statusCode);
-          expect(response.statusCode).toBe(200);
-          const result = await userTitleRepository.findOne({
-            where: { user: { id: user.id }, isSelected: true },
-          });
-          // console.log(result);
-          expect(result.title.id).toBe(testService.titles[0].id);
-        });
-      });
-
-      describe('/users/{nickname}/achievements', () => {
-        it('achievements를 순서대로 선택한 경우', async () => {
-          const user: User = await testService.createUserWithCollectables();
-          const token = jwtService.sign({
-            id: user.id,
-            nickname: user.nickname,
-            roleType: user.roleType,
-          });
-
-          const response = await request(app.getHttpServer())
-            .patch('/users/' + user.nickname + '/achievements')
-            .set({ Authorization: 'Bearer ' + token })
-            .send({
-              achievements: [
-                testService.achievements[0].id,
-                testService.achievements[1].id,
-                testService.achievements[2].id,
-              ],
+              nickname: 'testNickname',
+              imgeUrl: 'testImageUrl',
+              title: testService.titles[9].id,
             });
 
-          // console.log(response.statusCode);
-          expect(response.statusCode).toBe(200);
-          const result = await userAchievementRepository.find({
-            where: { user: { id: user.id } },
-          });
-          // console.log(result);
-          expect(result[0].achievement.id).toBe(testService.achievements[0].id);
-          expect(result[1].achievement.id).toBe(testService.achievements[1].id);
-          expect(result[2].achievement.id).toBe(testService.achievements[2].id);
-        });
-
-        it('achievement을 임의의 순서대로 선택한경우', async () => {
-          const user: User =
-            await testService.createMixedSelectedAchievementUser();
-          const token = jwtService.sign({
-            id: user.id,
-            nickname: user.nickname,
-            roleType: user.roleType,
-          });
-          const response = await request(app.getHttpServer())
-            .patch('/users/' + user.nickname + '/achievements')
-            .set({ Authorization: 'Bearer ' + token })
-            .send({
-              achievements: [
-                testService.achievements[2].id,
-                null,
-                testService.achievements[3].id,
-              ],
-            });
-          // console.log(response.statusCode);
-          const result = await userAchievementRepository.find({
-            where: { user: { id: user.id }, selectedOrder: Not(IsNull()) },
-          });
-          // console.log('response', response.body);
-          expect(response.statusCode).toBe(200);
-          expect(result[0].achievement.id).toBe(testService.achievements[2].id);
-          expect(result[1].achievement.id).toBe(testService.achievements[3].id);
-
-          expect(result[0].selectedOrder).toBe(0);
-          expect(result[1].selectedOrder).toBe(2);
-        });
-
-        it('achievement을 선택하지 않은 경우 (전부 null)', async () => {
-          const user: User =
-            await testService.createUserWithUnSelectedAchievements();
-          const token = jwtService.sign({
-            id: user.id,
-            nickname: user.nickname,
-            roleType: user.roleType,
-          });
-          const response = await request(app.getHttpServer())
-            .patch('/users/' + user.nickname + '/achievements')
-            .set({ Authorization: 'Bearer ' + token })
-            .send({
-              achievements: [null, null, null],
-            });
-          // console.log(response.body);
-          const result = await userAchievementRepository.find({
-            where: { user: { id: user.id }, selectedOrder: Not(IsNull()) },
-          });
-          // console.log(result);
-          expect(response.statusCode).toBe(200);
-          expect(result.length).toBe(0);
-        });
-      });
-      describe('/users/{nickname}/emojis', () => {
-        it('emoji를 순서대로 선택한 경우', async () => {
-          const user: User = await testService.createUserWithCollectables();
-          const token = jwtService.sign({
-            id: user.id,
-            nickname: user.nickname,
-            roleType: user.roleType,
-          });
-
-          const response = await request(app.getHttpServer())
-            .patch('/users/' + user.nickname + '/emojis')
-            .set({ Authorization: 'Bearer ' + token })
-            .send({
-              emojis: [
-                testService.emojis[0].id,
-                testService.emojis[1].id,
-                testService.emojis[2].id,
-              ],
-            });
-
-          // console.log(response.statusCode);
-          expect(response.statusCode).toBe(200);
-          const result = await userEmojiRepository.find({
-            where: { user: { id: user.id } },
-          });
-          // console.log(result);
-          expect(result[0].emoji.id).toBe(testService.emojis[0].id);
-          expect(result[1].emoji.id).toBe(testService.emojis[1].id);
-          expect(result[2].emoji.id).toBe(testService.emojis[2].id);
-
-          expect(result[0].selectedOrder).toBe(0);
-          expect(result[1].selectedOrder).toBe(1);
-          expect(result[2].selectedOrder).toBe(2);
-        });
-
-        it('emoji를 임의의 순서대로 선택한경우', async () => {
-          const user: User = await testService.createMixedSelectedEmojiUser();
-          const token = jwtService.sign({
-            id: user.id,
-            nickname: user.nickname,
-            roleType: user.roleType,
-          });
-          const response = await request(app.getHttpServer())
-            .patch('/users/' + user.nickname + '/emojis')
-            .set({ Authorization: 'Bearer ' + token })
-            .send({
-              emojis: [
-                testService.emojis[2].id,
-                null,
-                testService.emojis[3].id,
-              ],
-            });
-
-          const result = await userEmojiRepository.find({
-            where: { user: { id: user.id }, selectedOrder: Not(IsNull()) },
-          });
-          expect(response.statusCode).toBe(200);
-          expect(result[0].emoji.id).toBe(testService.emojis[2].id);
-          expect(result[1].emoji.id).toBe(testService.emojis[3].id);
-
-          expect(result[0].selectedOrder).toBe(0);
-          expect(result[1].selectedOrder).toBe(2);
-        });
-
-        it('emoji를 선택하지 않은 경우 (전부 null)', async () => {
-          const user: User = await testService.createUserWithUnSelectedEmojis();
-          const token = jwtService.sign({
-            id: user.id,
-            nickname: user.nickname,
-            roleType: user.roleType,
-          });
-          const response = await request(app.getHttpServer())
-            .patch('/users/' + user.nickname + '/emojis')
-            .set({ Authorization: 'Bearer ' + token })
-            .send({
-              emojis: [null, null, null],
-            });
-
-          const result = await userEmojiRepository.find({
-            where: { user: { id: user.id }, selectedOrder: Not(IsNull()) },
-          });
-          expect(response.statusCode).toBe(200);
-          expect(result.length).toBe(0);
+          console.log(response.body);
+          expect(response.statusCode).toBe(400);
         });
       });
     });
