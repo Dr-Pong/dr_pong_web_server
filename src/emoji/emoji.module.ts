c014b18bc5070a90058f64e3782912c78ff8ac1f
NamJun Heo <90181905+nheo9143@users.noreply.github.com>
2023-05-03 16:54:07 +0900

    [Refactor] 사용하지 않는 코드 제거

--- src/emoji/emoji.module.ts
+++ src/emoji/emoji.module.ts
@@ -1,7 +1,4 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { Emoji } from './emoji.entity';
-import { EmojiRepository } from './emoji.repository';
 
 @Module({})
 export class EmojiModule {}
