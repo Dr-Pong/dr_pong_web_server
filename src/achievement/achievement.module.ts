178db07b187a7c1b9632b44cfdc5fddc1cc35258
NamJun Heo <90181905+nheo9143@users.noreply.github.com>
2023-05-03 16:53:46 +0900

    [Refactor] 사용하지 않는 코드 제거

--- src/achievement/achievement.module.ts
+++ src/achievement/achievement.module.ts
@@ -1,7 +1,4 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { Achievement } from './achievement.entity';
-import { AchievementRepository } from './achievement.repository';
 
 @Module({})
 export class AchievementModule {}
