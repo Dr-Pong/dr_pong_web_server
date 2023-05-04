a6284ca506811eaf003499b72b9ef0a251135dc2
ManOfDaemosan <hwanee0000@gmail.com>
2023-05-04 16:28:10 +0900

    [Fix / Service] 서비스로직 및 레포지토리 로직 수정 #99

--- src/rank/rank.repository.ts
+++ src/rank/rank.repository.ts
@@ -1,22 +1,45 @@
-import { Injectable } from "@nestjs/common";
-import { InjectRepository } from "@nestjs/typeorm";
-import { Repository } from "typeorm";
-import { Rank } from "./rank.entity";
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Rank } from './rank.entity';
+import { GetRanksTopDto } from './dto/get.ranks.top.count.dto';
+import { GetRanksBottomDto } from './dto/get.ranks.bottom.dto';
 
 @Injectable()
 export class RankRepository {
-	constructor(
-		@InjectRepository(Rank)
-		private readonly repository: Repository<Rank>,
-	) {}
+  constructor(
+    @InjectRepository(Rank)
+    private readonly repository: Repository<Rank>,
+  ) {}
 
-	async findByUserIdAndSeasonId(userId:number, seasonId:number): Promise<Rank> {
-		return await this.repository.findOne({where:{user:{id:userId}, season:{id:seasonId}}});
-	}
+  async findByUserIdAndSeasonId(
+    userId: number,
+    seasonId: number,
+  ): Promise<Rank> {
+    return await this.repository.findOne({
+      where: { user: { id: userId }, season: { id: seasonId } },
+    });
+  }
 
-	async findHighestRankByUserId(userId:number): Promise<Rank> {
-		return await this.repository.findOne({where:{user:{id:userId}}, order:{highestPoint:'ASC'}});
-	}
+  async findHighestRankByUserId(userId: number): Promise<Rank> {
+    return await this.repository.findOne({
+      where: { user: { id: userId } },
+      order: { highestPoint: 'ASC' },
+    });
+  }
 
-	
+  async findTopRanksByCount(getDto: GetRanksTopDto): Promise<Rank[]> {
+    return await this.repository.find({
+      take: getDto.count,
+      order: { ladderPoint: 'ASC' },
+    });
+  }
+
+  async findBottomRanksByCount(getDto: GetRanksBottomDto): Promise<Rank[]> {
+    return await this.repository.find({
+      take: getDto.count,
+      skip: getDto.offset,
+      order: { ladderPoint: 'ASC' },
+    });
+  }
 }
