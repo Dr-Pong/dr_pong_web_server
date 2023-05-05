1b5c5f7f2a5fa75482d4d5f2272dba2dff5a12f2
Heo Namjun <heonamjun@Heos-MacBook-Air.local>
2023-05-05 15:55:18 +0900

    [Fix / Api] 명세에 안맞는 부분 수정 #93

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -50,7 +50,7 @@ export class UserController {
     private userAchievementService: UserAchievementService,
     private userEmojiService: UserEmojiService,
     private userTitleService: UserTitleService,
-  ) {}
+  ) { }
 
   @Get('/:nickname/detail')
   async userDetailByNicknameGet(@Param('nickname') nickname: string) {
@@ -93,11 +93,11 @@ export class UserController {
     };
     const achievements = selected
       ? await this.userAchievementService.getUserAchievementsSelected(
-          getUserAchievementDto,
-        )
+        getUserAchievementDto,
+      )
       : await this.userAchievementService.getUserAchievementsAll(
-          getUserAchievementDto,
-        );
+        getUserAchievementDto,
+      );
     const responseDto: UserAchievementsResponseDto = {
       achievements: achievements.achievements,
     };
@@ -163,7 +163,7 @@ export class UserController {
     );
     const patchUserTitleDto: PatchUserTitleDto = {
       userId: userInfoDto.id,
-      titleId: patchRequestDto.titleId,
+      titleId: patchRequestDto.title,
     };
     await this.userTitleService.patchUserTitle(patchUserTitleDto);
   }
@@ -182,14 +182,14 @@ export class UserController {
     );
     const patchUserImageDto: PatchUserImageDto = {
       userId: userInfoDto.id,
-      imageId: patchRequestDto.imageId,
+      imageId: patchRequestDto.imgId,
     };
     await this.userService.patchUserImage(patchUserImageDto);
   }
 
   /** Patch User StatusMessage*/
   @UseGuards(AuthGuard('jwt'))
-  @Patch('/:nickname/image')
+  @Patch('/:nickname/message')
   async usersMessageByNicknamePatch(
     @Param('nickname') nickname: string,
     @Body()
