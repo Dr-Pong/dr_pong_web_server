bd50d89dc1724ba58491b1151ec319efb3383148
Heo Namjun <heonamjun@Heos-MacBook-Air.local>
2023-05-09 20:47:42 +0900

    [Fix] UserGame에 필요한 데이터 생성 함수 수정 #42

--- src/test/test.service.ts
+++ src/test/test.service.ts
@@ -465,11 +465,18 @@ export class TestService {
     }
     for (let i = 0; i < n; i++) {
       await this.userGameRepository.save({
-        user: users[i % 2],
+        user: users[0],
         game: this.games[i],
-        result: i % 2 === 0 ? GAMERESULT_WIN : GAMERESULT_LOSE,
-        score: i % 2 === 0 ? 10 : 0,
-        lpChange: this.games[i].type === GAMETYPE_RANK ? (i % 2 === 0 ? 10 : -10) : 0,
+        result: GAMERESULT_WIN,
+        score: 10,
+        lpChange: this.games[i].type === GAMETYPE_RANK ? 10 : 0,
+      })
+      await this.userGameRepository.save({
+        user: users[1],
+        game: this.games[i],
+        result: GAMERESULT_LOSE,
+        score: 0,
+        lpChange: this.games[i].type === GAMETYPE_RANK ? -10 : 0,
       })
     }
   }
