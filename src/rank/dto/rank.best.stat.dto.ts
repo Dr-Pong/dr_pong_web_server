beffe0c73f876762191004e01d9074aca9956144
ManOfDaemosan <hwanee0000@gmail.com>
2023-05-09 16:05:05 +0900

    [Fix] dto로 함수 빼기 대작전#118

--- src/rank/dto/rank.best.stat.dto.ts
+++ src/rank/dto/rank.best.stat.dto.ts
@@ -1,5 +1,31 @@
+import { TIER_DOCTOR, TIER_EGG, TierType } from 'src/global/type/type.tier';
+
 export class RankBestStatDto {
   record: number | null;
   rank: number | null;
   tier: string;
+
+  static eggUser(): RankBestStatDto {
+    return {
+      record: null,
+      rank: null,
+      tier: TIER_EGG,
+    };
+  }
+
+  static nonDoctorUser(record: number, tier: TierType): RankBestStatDto {
+    return {
+      record,
+      rank: null,
+      tier,
+    };
+  }
+
+  static doctorUser(record: number, rank: number): RankBestStatDto {
+    return {
+      record,
+      rank,
+      tier: TIER_DOCTOR,
+    };
+  }
 }
