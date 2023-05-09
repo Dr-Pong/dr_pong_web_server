a59524e3fa1691f54d2bc563d3630b2999588db5
Heo Namjun <heonamjun@Heos-MacBook-Air.local>
2023-05-09 11:21:19 +0900

    [Fix] .env 파일 설정 읽어오도록 main문에 추가

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { initializeTransactionalContext } from 'typeorm-transactional';
 import { AppModule } from './app.module';
+import * as dotenv from 'dotenv';
 
 async function bootstrap() {
   initializeTransactionalContext();
+  dotenv.config();
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin: '*',
