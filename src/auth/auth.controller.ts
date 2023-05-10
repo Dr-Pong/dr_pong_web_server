d7a5ea69e3ba053ff07db687caf15826408a4704
Heo Namjun <heonamjun@Heos-MacBook-Air.local>
2023-05-10 17:46:48 +0900

    [Feat] 회원가입 컨트롤러 구현 #67

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,20 +1,31 @@
-import { Body, Controller, Get, Post, Query } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Query, Req } from '@nestjs/common';
 import axios from 'axios';
 import { AuthService } from './auth.service';
 import { AuthDto } from './dto/auth.dto';
 import { RoleType } from 'src/global/type/type.user.roletype';
+import { SignUpRequestDto } from './dto/auth.signup.request.dto';
 
 @Controller('auth')
 export class AuthController {
-	constructor(private authService: AuthService,) {}
+	constructor(private authService: AuthService,) { }
 
 	@Post('/42')
-	async fortyTwoLogin(@Body('authCode') code:string) : Promise<JwtDto> {
+	async fortyTwoLogin(@Body('authCode') code: string): Promise<JwtDto> {
 		console.log(code);
-		const accessToken : string = await this.authService.getFTAccessToken(code);
-		const userInfo : AuthDto = await this.authService.getFTUserInfo(accessToken);
-		const jwt : string = await this.authService.createJwtFromUser(userInfo);
+		const accessToken: string = await this.authService.getFTAccessToken(code);
+		const userInfo: AuthDto = await this.authService.getFTUserInfo(accessToken);
+		const jwt: string = await this.authService.createJwtFromUser(userInfo);
 		console.log(jwt);
-		return {token: jwt};
+		return { token: jwt };
+	}
+
+	@Post('signup')
+	async signUp(@Body() body: SignUpRequestDto, @Req() req) {
+		const userId: number = req.user.id;
+		await this.authService.signUp({
+			userId,
+			nickname: body.nickname,
+			imageId: body.imgId,
+		});
 	}
 }
