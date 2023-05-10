972140ddcaed7e3aaaaeec25cf7941ee1b729f1c
Heo Namjun <heonamjun@Heos-MacBook-Air.local>
2023-05-10 17:36:35 +0900

    [Fix] 2차 인증 관련 변수 추가 #67

--- src/auth/jwt/jwt.token.interface.ts
+++ src/auth/jwt/jwt.token.interface.ts
@@ -1,9 +1,10 @@
 import { RoleType } from "src/global/type/type.user.roletype";
 
 export class TokenInterface {
-	id:number;
-	nickname:string;
-	roleType:RoleType;
+	id: number;
+	nickname: string;
+	roleType: RoleType;
+	secondAuthRequierd: boolean;
 	imp: number;
 	exp: number;
 }
