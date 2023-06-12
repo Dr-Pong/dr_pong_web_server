7e1abacee056fd19ab7f64e56e3398895765c3a4
ManOfDaemosan <hwanee0000@gmail.com>
2023-06-12 13:43:42 +0900

    [Fix] requester 데코레이터 수정

--- src/domain/auth/jwt/auth.requestor.decorator.ts
+++ src/domain/auth/jwt/auth.requestor.decorator.ts
@@ -3,6 +3,10 @@ import { ExecutionContext, createParamDecorator } from '@nestjs/common';
 export const Requestor = createParamDecorator(
   (data: unknown, ctx: ExecutionContext) => {
     const request = ctx.switchToHttp().getRequest();
-    return request.user;
+    const user = {
+      id: Number(request.user.id),
+      nickname: request.user.nickname,
+    };
+    return user;
   },
 );
