3887b8f61ecd945a0805ae7262c0a39f1fd8d72c
ManOfDaemosan <hwanee0000@gmail.com>
2023-07-04 14:57:33 +0900

    [Fix] 서비스 겜 결과 저장 추가 #185

--- src/domain/game/game.service.ts
+++ src/domain/game/game.service.ts
@@ -7,6 +7,12 @@ import { TouchLogRepository } from '../touch-log/touch.log.repository';
 import { SeasonRepository } from '../season/season.repository';
 import { Season } from '../season/season.entity';
 import { UserGame } from '../user-game/user-game.entity';
+import {
+  GAMERESULT_LOSE,
+  GAMERESULT_TIE,
+  GAMERESULT_WIN,
+  GameResultType,
+} from 'src/global/type/type.game.result';
 
 @Injectable()
 export class GameService {
@@ -19,7 +25,6 @@ export class GameService {
 
   async postGame(postGameDto: PostGameDto): Promise<void> {
     const { player1, player2 } = postGameDto;
-
     const currentSeason: Season =
       await this.seasonRepository.findCurrentSeason();
 
@@ -27,20 +32,46 @@ export class GameService {
       postGameDto,
       currentSeason,
     );
+    const player1Result = this.checkGameResult(
+      postGameDto.player1.score,
+      postGameDto.player2.score,
+    );
+    const player2Result = this.checkGameResult(
+      postGameDto.player2.score,
+      postGameDto.player1.score,
+    );
 
     const userGame1: UserGame = await this.userGameRepository.save(
       player1,
       game,
+      player1Result,
     );
     const userGame2: UserGame = await this.userGameRepository.save(
       player2,
       game,
+      player2Result,
     );
 
     const logs = postGameDto.logs;
     for (const log of logs) {
-      const user = player1.id === log.userId ? userGame1.user : userGame2.user;
-      await this.touchLogRepository.save(user, log.event, log.round, log.ball);
+      const userGame: UserGame =
+        player1.id === log.userId ? userGame1 : userGame2;
+      await this.touchLogRepository.save(
+        userGame,
+        log.event,
+        log.round,
+        log.ball,
+      );
+    }
+  }
+
+  checkGameResult(player1Score: number, player2Score: number): GameResultType {
+    if (player1Score > player2Score) {
+      return GAMERESULT_WIN;
+    } else if (player1Score < player2Score) {
+      return GAMERESULT_LOSE;
+    } else {
+      return GAMERESULT_TIE;
     }
   }
 }
