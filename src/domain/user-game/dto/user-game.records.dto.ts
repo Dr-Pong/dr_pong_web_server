b765da22d9c970f21086f2b5e411f6968e78d624
ManOfDaemosan <hwanee0000@gmail.com>
2023-07-12 18:12:06 +0900

    [Fix] 게임결과 승패 오류 수정 #185

--- src/domain/user-game/dto/user-game.records.dto.ts
+++ src/domain/user-game/dto/user-game.records.dto.ts
@@ -20,13 +20,16 @@ export class UserGameRecordDto {
     for (let i = 0; i < userGames.length / 2; i++) {
       let me: GamePlayerDto;
       let you: GamePlayerDto;
+      let result: GameResultType;
 
       if (userGames[i * 2].user.id === userId) {
         me = GamePlayerDto.formUserGame(userGames[i * 2]);
         you = GamePlayerDto.formUserGame(userGames[i * 2 + 1]);
+        result = userGames[i * 2].result;
       } else {
         me = GamePlayerDto.formUserGame(userGames[i * 2 + 1]);
         you = GamePlayerDto.formUserGame(userGames[i * 2]);
+        result = userGames[i * 2 + 1].result;
       }
 
       records.push({
@@ -35,7 +38,7 @@ export class UserGameRecordDto {
         playedAt: userGames[i * 2].game.startTime.toString(),
         me: me,
         you: you,
-        result: userGames[i * 2].result,
+        result: result,
       });
     }
     return records;
