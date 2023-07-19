2528d12b5756772f3c3880a871d23651b4fba843
ManOfDaemosan <hwanee0000@gmail.com>
2023-07-19 15:45:49 +0900

    [Fix] 비동기 await 안하고 있던부분 추가 #195

--- src/domain/game/game.service.ts
+++ src/domain/game/game.service.ts
@@ -301,14 +301,14 @@ export class GameService {
     const player1Level = this.calculateLevel(player1, player1Exp);
     await this.userRepository.update(player1.id, player1Exp, player1Level);
     const updatedPlayer1: User = await this.userRepository.findById(player1Id);
-    this.updateTitle(updatedPlayer1, titles);
+    await this.updateTitle(updatedPlayer1, titles);
 
     // player2의 레벨 업 체크 및 경험치 추가
     const player2Exp = this.calculateExperiencePoints(player2Result);
     const player2Level = this.calculateLevel(player2, player2Exp);
     await this.userRepository.update(player2.id, player2Exp, player2Level);
     const updatedPlayer2: User = await this.userRepository.findById(player2Id);
-    this.updateTitle(updatedPlayer2, titles);
+    await this.updateTitle(updatedPlayer2, titles);
 
     // 업데이트된 유저 타이틀 정보를 반환합니다.
     const updateUserTitles: UpdateUserTitlesDto = new UpdateUserTitlesDto();
