28655566f0648c08d1e51d2db27d432469c356bd
nheo9143 <nheo@student.42seoul.kr>
2023-09-20 10:53:59 +0900

    [Feat] 시즌 시작시 모든 유저의 랭크 정보 초기화하도록 수정 #242

--- src/domain/season/season.service.ts
+++ src/domain/season/season.service.ts
@@ -1,13 +1,25 @@
 import { Injectable } from '@nestjs/common';
 import { SeasonRepository } from './season.repository';
 import { Cron } from '@nestjs/schedule';
+import { UserRepository } from '../user/user.repository';
+import { Season } from './season.entity';
+import { User } from '../user/user.entity';
+import { RankRepository } from '../rank/rank.repository';
 
 @Injectable()
 export class SeasonService {
-  constructor(private readonly seasonRepository: SeasonRepository) {}
+  constructor(
+    private readonly seasonRepository: SeasonRepository,
+    private readonly userRepository: UserRepository,
+    private readonly rankRepository: RankRepository,
+  ) {}
 
   @Cron('0 0 0 * * MON')
   async createSeason(): Promise<void> {
-    await this.seasonRepository.save();
+    const newSeason: Season = await this.seasonRepository.save();
+    const users: User[] = await this.userRepository.findAll();
+    for (const user of users) {
+      await this.rankRepository.save(user.id, newSeason.id);
+    }
   }
 }
